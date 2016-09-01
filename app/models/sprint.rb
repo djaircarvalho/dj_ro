e446f6d4fcf18d3d0661a01ed325375afd4fdff9
Djair <djaircarvalho.dj7@gmail.com>
2016-09-01 17:00:11 -0300

    Adicionado relacionamentos entre todo o modelo

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,3 +1,4 @@
 class Sprint < ApplicationRecord
   belongs_to :project
+  has_many :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830195313) do
+ActiveRecord::Schema.define(version: 20160901190755) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
