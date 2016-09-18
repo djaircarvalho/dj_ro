d2d15147347c1a66b7b6e2fbc901c82eb794a5e8
Djair <djaircarvalho.dj7@gmail.com>
2016-09-18 10:14:48 -0300

    Modificado routes.rb para aninhar tasks em sprints e sprints em projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'projects#index'
-  resources :tasks
-  resources :sprints
-  resources :projects
+  
+  resources :projects do
+    resources :sprints do
+        resources :tasks
+    end
+  end
   resources :teams
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
