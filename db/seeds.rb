82c14f6ae0b6090ada863a5d633bbe053405e0d8
Djair <djaircarvalho.dj7@gmail.com>
2016-09-19 22:31:41 -0300

    corrigido erro ao mostrar a closing_date no index de sprints que na verdade estava mostrando o opening_date e aficioando algumas informacoes no seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,17 @@
 djair = User.create!(name: 'Djair Carvalho', email: 'djaircarvalho.dj7@gmail.com', password: '12345678',  admin:true)
 rosa = User.create!(name: 'Rosemberg de Santana', email: 'rosembergdesantana@gmail.com', password: '12345678',  admin:false)
 
-proj = Project.create!(name: "Cooperativa de Animais Abandonados", opening_date: Time.zone.local(2011,4,12,14,0), closing_date:Time.zone.local(2014,4,12,14,0))
+
+proj2 = Project.create!(name: "DjRo", opening_date: Time.zone.local(2016,7,12,14,0), closing_date: Time.zone.local(2016,10,12,14,0), expected_value: 800 )
+proj3 = Project.create!(name: "Topicos Especiais", opening_date: Time.zone.local(2016,7,12,14,0), closing_date: Time.zone.local(2016,11,12,14,0),  expected_value: 800)
+
+
+proj2.users << djair << rosa
+proj3.users << djair << rosa
+
+sp1 = Sprint.create!(name: "Agosto (2016)", opening_date: Time.zone.local(2016,8,1,14,0), closing_date: Time.zone.local(2016,9,1,14,0))
+sp2 = Sprint.create!(name: "Setembro (2016)", opening_date: Time.zone.local(2016,9,1,14,0), closing_date: Time.zone.local(2016,10,1,14,0))
+sp3 = Sprint.create!(name: "Agosto (2016)", opening_date: Time.zone.local(2016,8,1,14,0), closing_date: Time.zone.local(2016,9,1,14,0))
+
+proj2.sprints << sp1 << sp2
+proj3.sprints << sp3
